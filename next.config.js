cbdd803d1fd88d5d2cfa15feac41c7e47a2d5c8c
Levi Araújo <leviprudenciont@gmail.com>
2022-10-22 21:01:29 -0300

    chore: update dependencies

--- next.config.js
+++ next.config.js
@@ -1,13 +1,21 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const withPWA = require('next-pwa');
-
 const isProduction = process.env.NODE_ENV === 'production';
-module.exports = withPWA({
-	pwa: {
-		dest: 'public',
-		disable: !isProduction
+
+/**
+ * @type {import('next').NextConfig}
+ */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({
+	dest: 'public',
+	disable: !isProduction
+});
+
+const nextConfig = withPWA({
+	experimental: {
+		forceSwcTransforms: true
 	},
 	future: {
-    webpack5: true,
-  },
+		webpack5: true
+	}
 });
+
+module.exports = nextConfig;
